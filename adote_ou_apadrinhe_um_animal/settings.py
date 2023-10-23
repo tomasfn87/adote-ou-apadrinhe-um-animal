bae07b5a7a1b90e79b73fd9f784c623bfd87ba76
Tomás Foch Nalle <tomas.nalle@alumni.usp.br>
2023-10-22 22:17:45 -0300

    Removida função 'load_env_file' (por ser usada uma única vez).

--- adote_ou_apadrinhe_um_animal/settings.py
+++ adote_ou_apadrinhe_um_animal/settings.py
@@ -13,14 +13,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from os import environ as env
 
-def load_env_file():
-    with open(".env", "r") as env_file:
-        for line in env_file:
-            if "=" in line:
-                key, value = line.strip().split("=", 1)
-                env[key] = value
-
-load_env_file()
+with open(".env", "r") as env_file:
+    for line in env_file:
+        if "=" in line:
+            key, value = line.strip().split("=", 1)
+            env[key] = value
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
